ce32b39151f81785be34fe4926549546b28c329d
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-03 17:22:40 +0100

    subnet1

--- public-subnet.tf
+++ public-subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.aws_capstone_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "PublicSubnet1"
