580b20f73c7032c3d80af6102b3c89fb35ab26b4
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-03 15:17:19 +0100

    Initial commit: Terraform starter project

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 terraform {
-    required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.29.0"
-    }
+ required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 6.0"
   }
-}
+ }
+
+ }
