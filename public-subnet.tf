940d89c1540ad5105a10ea3b134aac0140096f2b
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-03 17:05:42 +0100

    add subnet

--- public-subnet.tf
+++ public-subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.aws_capstone_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = var.aws_availability_zone_a
+  availability_zone       = "us-west-2"
   map_public_ip_on_launch = true
   tags = {
     Name = "PublicSubnet1"
