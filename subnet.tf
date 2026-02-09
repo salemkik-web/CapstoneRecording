d69b9a4e3d6ce0585abafe968338680faf85aeda
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-09 18:28:16 +0100

    new sun

--- subnet.tf
+++ subnet.tf
@@ -1,18 +1,19 @@
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr[0]  # pick the first element
   map_public_ip_on_launch = true
   tags = { Name = "public-subnet" }
 }
 
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr
+  cidr_block = var.private_subnet_cidr[0]  # pick the first element
   tags = { Name = "private-subnet" }
 }
+
 resource "aws_db_subnet_group" "wordpress" {
   name       = "wordpress-db-subnet-group"
-  subnet_ids = [aws_subnet.private.id]
+  subnet_ids = [aws_subnet.private.id]   # still works because it's one subnet
 
   tags = {
     Name = "WordPress DB Subnet Group"
