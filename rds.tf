232c8d9054f5a032a821e3b8e614a4eb71bcbf38
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-09 18:20:53 +0100

    new instanet

--- rds.tf
+++ rds.tf
@@ -7,11 +7,13 @@ resource "aws_db_instance" "wordpress" {
 
   username          = var.db_user
   password          = var.db_password
-  db_name           = var.db_name   # correct attribute
-  parameter_group_name = "default.mariadb10.5"
+  db_name           = var.db_name
+
+  # Correct argument name for subnet group
+  db_subnet_group_name = aws_db_subnet_group.wordpress.name
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
   publicly_accessible = false
   skip_final_snapshot = true
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  subnet_group_name      = aws_db_subnet_group.wordpress.name
 }
