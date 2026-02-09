9ffaddad1c4f6977dbf2a5dbf75d0d2822197c31
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-09 14:06:15 +0100

    2

--- bastion.tf
+++ bastion.tf
@@ -1,7 +1,18 @@
+# Fetch latest Amazon Linux 2 AMI
+data "aws_ami" "amzn2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.amzn2.id
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public.id
+  ami           = data.aws_ami.amzn2.id   # <- use the correct data source name
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public[0].id
   key_name      = var.key_name
   security_groups = [aws_security_group.bastion_sg.id]
 
