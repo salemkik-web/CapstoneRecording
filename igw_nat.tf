24bbfafcdb223800b87802d384c2d2d918413a13
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-09 19:50:06 +0100

    new

--- igw_nat.tf
+++ igw_nat.tf
@@ -3,12 +3,20 @@ resource "aws_internet_gateway" "igw" {
   tags = { Name = "lab-igw" }
 }
 
-resource "aws_eip" "nat" {
-  vpc = true
-}
+
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
-  tags = { Name = "lab-nat" }
+  tags = { Name = "nat-gateway" }
+}
+
+
+
+
+resource "aws_eip" "nat" {
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name = "nat-eip"
+  }
 }
