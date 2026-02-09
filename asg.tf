63ca7643389eab657e2f4b5efc7911db27702fd3
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-09 10:30:31 +0100

    new changes

--- asg.tf
+++ asg.tf
@@ -1,45 +1,89 @@
-data "aws_ami" "amazon" {
+#############################################
+# AMI
+#############################################
+
+data "aws_ami" "amazon_linux" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
 
   filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-*"]
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 
-resource "aws_launch_template" "lt" {
-  image_id = data.aws_ami.amazon.id
+#############################################
+# Launch Template
+#############################################
+
+resource "aws_launch_template" "wp_lt" {
+  name_prefix   = "wp-lt-"
+  image_id      = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-  user_data = filebase64("userdata.sh")
+
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   iam_instance_profile {
     name = aws_iam_instance_profile.profile.name
   }
-}
 
-resource "aws_autoscaling_group" "asg" {
-  desired_capacity = 1
-  min_size = 1
-  max_size = 2
-  vpc_zone_identifier = aws_subnet.private[*].id
-  target_group_arns = [aws_lb_target_group.tg.arn]
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    db_name     = var.db_name
+    db_user     = var.db_user
+    db_password = var.db_password
+    db_host     = aws_db_instance.db.address
+  }))
 
-  launch_template {
-    id = aws_launch_template.lt.id
-    version = "$Latest"
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "wordpress-asg"
+    }
   }
 }
 
-resource "aws_autoscaling_policy" "cpu_policy" {
-  policy_type = "TargetTrackingScaling"
-  autoscaling_group_name = aws_autoscaling_group.asg.name
+#############################################
+# Auto Scaling Group
+#############################################
+
+resource "aws_autoscaling_group" "wp_asg" {
+  name                = "wp-asg"
+  desired_capacity    = 1
+  min_size            = 1
+  max_size            = 3
+  vpc_zone_identifier = aws_subnet.private[*].id
+
+  target_group_arns = [aws_lb_target_group.tg.arn]
+
+  launch_template {
+    id      = aws_launch_template.wp_lt.id
+    version = "$Latest"
+  }
+
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  tag {
+    key                 = "Name"
+    value               = "wordpress-asg"
+    propagate_at_launch = true
+  }
+}
+
+#############################################
+# Auto Scaling Policy (CPU > 50%)
+#############################################
+
+resource "aws_autoscaling_policy" "cpu_target_tracking" {
+  name                   = "cpu-target-tracking"
+  autoscaling_group_name = aws_autoscaling_group.wp_asg.name
+  policy_type            = "TargetTrackingScaling"
 
   target_tracking_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
-    target_value = 50
+
+    target_value = 50.0
   }
 }
