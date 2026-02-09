f72897f2c2169dc9890f37c131ba588dc87de652
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-09 11:51:18 +0100

    wordpress

--- userdata.sh
+++ userdata.sh
@@ -8,11 +8,13 @@ systemctl enable httpd
 cd /var/www/html
 wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
-chown -R apache:apache wordpress
+mv wordpress/* .
+rm -rf wordpress latest.tar.gz
 
-cd wordpress
+chown -R apache:apache /var/www/html
+
+# configure WordPress
 cp wp-config-sample.php wp-config.php
-
 sed -i "s/database_name_here/${db_name}/" wp-config.php
 sed -i "s/username_here/${db_user}/" wp-config.php
 sed -i "s/password_here/${db_password}/" wp-config.php
