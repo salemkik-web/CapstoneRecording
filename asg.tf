320abdc9fab8cd377a81e489928eb63bc2295044
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-09 11:39:52 +0100

    ds

--- asg.tf
+++ asg.tf
@@ -1,3 +1,6 @@
+#############################
+# AMI Data
+#############################
 data "aws_ami" "amazon" {
   most_recent = true
   owners      = ["amazon"]
@@ -8,10 +11,18 @@ data "aws_ami" "amazon" {
   }
 }
 
+#############################
+# Launch Template
+#############################
 resource "aws_launch_template" "lt" {
+  name          = "wordpress-lt"
   image_id      = data.aws_ami.amazon.id
   instance_type = var.instance_type
+
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  # LAB-SAFE: REMOVE IAM instance profile block
+  # iam_instance_profile { name = "LabRole" } 
 
   user_data = base64encode(templatefile("${path.module}/userdata.sh", {
     db_name     = var.db_name
@@ -21,19 +32,35 @@ resource "aws_launch_template" "lt" {
   }))
 }
 
+#############################
+# Auto Scaling Group
+#############################
 resource "aws_autoscaling_group" "asg" {
-  desired_capacity    = 1
-  min_size            = 1
-  max_size            = 2
-  vpc_zone_identifier = aws_subnet.private[*].id
-  target_group_arns   = [aws_lb_target_group.tg.arn]
+  name                      = "wordpress-asg"
+  desired_capacity           = 1
+  min_size                   = 1
+  max_size                   = 2
+  vpc_zone_identifier        = aws_subnet.private[*].id
+  target_group_arns          = [aws_lb_target_group.tg.arn]
+  health_check_type          = "EC2"
+  health_check_grace_period  = 120
+  force_delete               = true
 
   launch_template {
     id      = aws_launch_template.lt.id
     version = "$Latest"
   }
+
+  tag {
+    key                 = "Name"
+    value               = "wordpress-instance"
+    propagate_at_launch = true
+  }
 }
 
+#############################
+# Auto Scaling Policy (CPU > 50%)
+#############################
 resource "aws_autoscaling_policy" "cpu" {
   name                   = "cpu-target-tracking-policy"
   policy_type            = "TargetTrackingScaling"
--- iam.tf
+++
@@ -1,5 +0,0 @@
-data "aws_iam_instance_profile" "lab_profile" {
-  name = "LabRole"  # Replace with the actual profile name
-}
-
-
