ae9ae5fde2bff237c621d70c8e1de6345864817c
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-10 13:06:32 +0100

    bastion-host

--- bastion.tf
+++ bastion.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.amazon_linux2.id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public1.id
   key_name      = var.key_name
   security_groups = [aws_security_group.bastion_sg.id]
 
