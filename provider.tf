e8f17c3a5cd11578d6891979fa3ef37c340624a1
salem elkikli <salemelkikli@salems-MBP.fritz.box>
2026-02-10 13:58:33 +0100

    new

--- provider.tf
+++ provider.tf
@@ -1,14 +1,13 @@
 terraform {
-  required_version = ">= 1.6"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.70.0"
     }
   }
 }
 
+# Creating a VPC with minimal settings
 provider "aws" {
- region = var.aws_region
+  region = var.aws_region
 }
